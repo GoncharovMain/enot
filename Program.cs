75cf6410135a42cb79d6d5b8ea0da290f002732d
Goncharov <goncharov.safe@gmail.com>
2022-10-31 23:18:28 +0300

    Added class Position.

--- Program.cs
+++ Program.cs
@@ -2,10 +2,22 @@ using System;
 
 namespace Enot
 {
+	public class Position
+	{
+		public double X { get; set; }
+		public double Y { get; set; }
+
+		public Position(double x, double y) => (X, Y) = (x, y);
+
+		public override string ToString() => $"X: {X} Y: {Y}";
+	}
+
 	public class Program
 	{
 		public static void Main()
 		{
+			Console.WriteLine(new Position(5.4, -4.2));
+
 			Console.WriteLine();
 		}
 	}
